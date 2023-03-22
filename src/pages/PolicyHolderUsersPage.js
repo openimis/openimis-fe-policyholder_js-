847571d6a0867200b08e16f38dab6455df5583fb
olewandowski <olewandowski@soldevelo.com>
2023-03-22 08:47:46 +0100

    OTC-757: Code refactor in PolicyHoldersPage and PolicyHolderUsersPage

--- src/pages/PolicyHolderUsersPage.js
+++ src/pages/PolicyHolderUsersPage.js
@@ -6,56 +6,66 @@ import { connect } from "react-redux";
 import PolicyHolderUserSearcher from "../components/PolicyHolderUserSearcher";
 import CreatePolicyHolderUserDialog from "../dialogs/CreatePolicyHolderUserDialog";
 import {
-    RIGHT_POLICYHOLDERUSER_SEARCH,
-    RIGHT_PORTALPOLICYHOLDERUSER_SEARCH,
-    RIGHT_POLICYHOLDERUSER_CREATE,
-    RIGHT_PORTALPOLICYHOLDERUSER_CREATE
-} from "../constants"
+  RIGHT_POLICYHOLDERUSER_SEARCH,
+  RIGHT_PORTALPOLICYHOLDERUSER_SEARCH,
+  RIGHT_POLICYHOLDERUSER_CREATE,
+  RIGHT_PORTALPOLICYHOLDERUSER_CREATE,
+} from "../constants";
 
-const styles = theme => ({
-    page: theme.page
-})
-
-class PolicyHolderUsersPage extends Component {
-    state = {
-        reset: 0
-    }
-
-    onSave = () =>
-        this.setState(state => ({
-            reset: state.reset + 1
-        }));
-
-    render() {
-        const { classes, rights } = this.props;
-        return (
-            [
-                RIGHT_POLICYHOLDERUSER_SEARCH,
-                RIGHT_PORTALPOLICYHOLDERUSER_SEARCH
-            ].some(right => rights.includes(right)) && (
-                <div className={classes.page}>
-                    <Helmet title={formatMessage(this.props.intl, "policyHolder", "menu.policyHolderUsers")} />
-                    <PolicyHolderUserSearcher
-                        rights={rights}
-                        reset={this.state.reset}
-                        onSave={this.onSave}
-                    />
-                    {[
-                        RIGHT_POLICYHOLDERUSER_CREATE,
-                        RIGHT_PORTALPOLICYHOLDERUSER_CREATE
-                    ].some(right => rights.includes(right)) && (
-                        <CreatePolicyHolderUserDialog
-                            onSave={this.onSave}
-                        />
-                    )}
-                </div>
-            )
-        );
-    }
-}
-
-const mapStateToProps = state => ({
-    rights: !!state.core && !!state.core.user && !!state.core.user.i_user ? state.core.user.i_user.rights : []
+const styles = (theme) => ({
+  page: theme.page,
 });
 
-export default injectIntl(withTheme(withStyles(styles)(connect(mapStateToProps, null)(PolicyHolderUsersPage))));
+class PolicyHolderUsersPage extends Component {
+  state = {
+    reset: 0,
+  };
+
+  onSave = () =>
+    this.setState((state) => ({
+      reset: state.reset + 1,
+    }));
+
+  render() {
+    const { classes, rights } = this.props;
+    return (
+      [RIGHT_POLICYHOLDERUSER_SEARCH, RIGHT_PORTALPOLICYHOLDERUSER_SEARCH].some(
+        (right) => rights.includes(right)
+      ) && (
+        <div className={classes.page}>
+          <Helmet
+            title={formatMessage(
+              this.props.intl,
+              "policyHolder",
+              "menu.policyHolderUsers"
+            )}
+          />
+          <PolicyHolderUserSearcher
+            rights={rights}
+            reset={this.state.reset}
+            onSave={this.onSave}
+          />
+          {[
+            RIGHT_POLICYHOLDERUSER_CREATE,
+            RIGHT_PORTALPOLICYHOLDERUSER_CREATE,
+          ].some((right) => rights.includes(right)) && (
+            <CreatePolicyHolderUserDialog onSave={this.onSave} />
+          )}
+        </div>
+      )
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  rights:
+    !!state.core && !!state.core.user && !!state.core.user.i_user
+      ? state.core.user.i_user.rights
+      : [],
+});
+
+export default injectIntl(
+  withTheme(
+    withStyles(styles)(connect(mapStateToProps, null)(PolicyHolderUsersPage))
+  )
+);
