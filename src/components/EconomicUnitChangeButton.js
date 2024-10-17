f017cc95c26f9c49fc83a61416bc61c7e70aa7ee
olewandowski1 <olewandowski@soldevelo.com>
2024-10-17 14:45:02 +0200

    OM-335: delete text in eu modal, change the button trigger ui

--- src/components/EconomicUnitChangeButton.js
+++ src/components/EconomicUnitChangeButton.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { useSelector } from "react-redux";
+import { useSelector } from 'react-redux';
 
 import { makeStyles } from '@material-ui/core/styles';
-import { IconButton, Tooltip } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import { PinDrop } from '@material-ui/icons';
 
 import { useTranslations, useModulesManager } from '@openimis/fe-core';
@@ -11,14 +11,14 @@ import { MODULE_NAME, RIGHT_VIEW_EU_MODAL } from '../constants';
 const useStyles = makeStyles((theme) => ({
   button: {
     margin: theme.spacing(2),
-    color: theme.palette.secondary.main,
+    color: theme.palette.primary.main,
   },
 }));
 
 const EconomicUnitChangeButton = ({ onEconomicDialogOpen }) => {
   const classes = useStyles();
   const modulesManager = useModulesManager();
-  const rights = useSelector((store) => store.core.user?.i_user?.rights ?? [])
+  const rights = useSelector((store) => store.core.user?.i_user?.rights ?? []);
   const { formatMessage } = useTranslations(MODULE_NAME, modulesManager);
 
   const economicUnitConfig = modulesManager.getConf(
@@ -30,14 +30,15 @@ const EconomicUnitChangeButton = ({ onEconomicDialogOpen }) => {
   if (!economicUnitConfig || !rights.includes(RIGHT_VIEW_EU_MODAL)) return null;
 
   return (
-    <Tooltip title={formatMessage('EconomicUnitChangeButton.tooltip')}>
-      <IconButton
-        className={classes.button}
-        onClick={() => onEconomicDialogOpen()}
-      >
-        <PinDrop />
-      </IconButton>
-    </Tooltip>
+    <Button
+      variant='contained'
+      color='secondary'
+      startIcon={<PinDrop />}
+      className={classes.button}
+      onClick={() => onEconomicDialogOpen()}
+    >
+      <strong> {formatMessage('EconomicUnitChangeButton.label')} </strong>
+    </Button>
   );
 };
 
